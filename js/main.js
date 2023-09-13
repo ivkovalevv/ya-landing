5a4e4f267a9099ee55c73ee0f4b85f30e4f50ce8
ivkovalevv <ivkovalevv@gmail.com>
2023-09-13 22:35:29 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ document.addEventListener('DOMContentLoaded', function(){
     let offset = 0;
     const sliderWrapper = document.querySelector('.section-exclusive__wrapper');
     const sliderTrack = document.querySelector('.section-exclusive__list');
-    let sliderWidth = sliderWrapper.clientWidth + 2;
+    let sliderWidth = sliderWrapper.clientWidth;
 
     const btnPrev = document.getElementById('btn-prev');
     const btnNext = document.getElementById('btn-next');
+
+    console.log(sliderWidth)
 
     btnPrev.disabled = true
 
     btnNext.addEventListener('click', function() {
         btnPrev.disabled = false
         offset += sliderWidth
+        console.log(offset)
         if (offset >= sliderWidth * 3) {
             btnNext.disabled = true
         }
-        sliderTrack.style.left = -offset -10 + 'px';
+        sliderTrack.style.left = -offset + 'px';
     })
 
     btnPrev.addEventListener('click', function() {
@@ -24,6 +27,6 @@ document.addEventListener('DOMContentLoaded', function(){
         if (offset <= 0) {
             btnPrev.disabled = true
         }
-        sliderTrack.style.left = -offset - 10 + 'px';
+        sliderTrack.style.left = -offset + 'px';
     })
 })
